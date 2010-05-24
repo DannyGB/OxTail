623a14804c426f74c452571401ead4b1b08e53e4
dannygb <dannygb@7887d560-b91c-4884-a66a-915123185056>
2010-05-24 08:14:52 +0000

    Commit including the newly re-factored helper project
    added      OxTailHelpers
    added      OxTailHelpers/bin
    added      OxTailHelpers/bin/Debug
    added      OxTailHelpers/bin/Debug/OxTailHelpers.dll
    added      OxTailHelpers/bin/Debug/OxTailHelpers.pdb
    added      OxTailHelpers/FileHelper.cs
    added      OxTailHelpers/obj
    added      OxTailHelpers/obj/Debug
    added      OxTailHelpers/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    added      OxTailHelpers/obj/Debug/OxTail.Helpers.csproj.FileListAbsolute.txt
    added      OxTailHelpers/obj/Debug/OxTailHelpers.dll
    added      OxTailHelpers/obj/Debug/OxTailHelpers.pdb
    added      OxTailHelpers/obj/Debug/TempPE
    added      OxTailHelpers/OxTail.Helpers.csproj
    added      OxTailHelpers/Properties
    added      OxTailHelpers/Properties/AssemblyInfo.cs

---
+++ OxTailHelpers/FileHelper.cs
@@ -0,0 +1,74 @@
+﻿/*****************************************************************
+* This file is part of OxTail.
+*
+* OxTail is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* OxTail is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with OxTail.  If not, see <http://www.gnu.org/licenses/>.
+* ********************************************************************/
+
+namespace OxTail.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.Win32;
+    using System.Windows;
+    using System.IO;
+    using System.Windows.Documents;
+    using System.Reflection;
+
+    public class FileHelper
+    {
+        public static string ShowOpenFileDialog()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            bool? retVal = dialog.ShowDialog();
+            if (retVal.HasValue && retVal.Value)
+            {
+                return dialog.FileName;
+            }
+
+            return string.Empty;
+        }
+
+        public static Stream OpenFile(string filename)
+        {
+            try
+            {
+                FileStream stream = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                stream.Position = 0;
+                return stream;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static FlowDocument CreateFlowDocument(Stream content)
+        {
+            FlowDocument fd = new FlowDocument();
+            TextRange textRange = new TextRange(fd.ContentStart, fd.ContentEnd);
+            textRange.Load(content, DataFormats.Rtf);
+
+            return fd;
+        }
+
+        public static Stream GetResourceStream(Assembly assembly, string resourceName)
+        {            
+            Stream s = assembly.GetManifestResourceStream(resourceName);
+
+            return s;
+        }
+    }
+}
