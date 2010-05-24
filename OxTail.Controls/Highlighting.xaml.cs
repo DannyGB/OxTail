bad9f8552eb5bdda1d7835496b30ddc17124efe4
Dan <dannygb@users.noreply.github.com>
2010-05-24 08:13:49 +0000

    Refacted helpers out into their own includeable project
    modified   OxTail.Controls/About.xaml.cs
    modified   OxTail.Controls/Highlighting.xaml.cs
    modified   OxTail.Controls/OxTail.Controls.csproj
    modified   OxTail.Controls/OxtailFileViewer.cs
    modified   OxTail.Controls/RecentFileList.cs
    modified   OxTail.sln
    modified   OxTail/MainWindow.xaml.cs
    modified   OxTail/OxTail.csproj
    deleted    OxTailLogic/Helpers
    deleted    OxTailLogic/Helpers/FileHelper.cs
    modified   OxTailLogic/OxTailLogic.csproj

--- OxTail.Controls/Highlighting.xaml.cs
+++ OxTail.Controls/Highlighting.xaml.cs
@@ -17,24 +17,10 @@
 
 namespace OxTail.Controls
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Windows;
     using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
-    using System.Windows.Input;
     using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
     using System.Collections.ObjectModel;
-    using System.Xml.Serialization;
-    using System.IO;
-    using System.Xml;
-    using OxTailLogic.PatternMatching;
 
     /// <summary>
     /// Interaction logic for Highlighting.xaml
