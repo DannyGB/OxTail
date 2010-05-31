7dd8ff97a695fed56c2157e7371bb48760602060
wedgberto <wedgberto@7887d560-b91c-4884-a66a-915123185056>
2010-05-31 22:23:39 +0000

    Started scrolling functionality. Window resize handling.

---
+++ OxTailHelpers/WpfHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace OxTailHelpers
+{
+    public static class WpfHelper
+    {
+        public static List<T> GetVisualChildren<T>(this Visual referenceVisual) where T : Visual
+        {
+            List<T> children = new List<T>();
+            Visual child = null;
+            for (Int32 i = 0; i < VisualTreeHelper.GetChildrenCount(referenceVisual); i++)
+            {
+                child = VisualTreeHelper.GetChild(referenceVisual, i) as Visual;
+                if (child != null)
+                {
+                    if (child is T)
+                    {
+                        children.Add(child as T);
+                    }
+                    List<T> subchildren = GetVisualChildren<T>(child);
+                    if (subchildren != null)
+                    {
+                        children.AddRange(subchildren);
+                    }
+                }
+            }
+            return children;
+        }
+    }
+}
