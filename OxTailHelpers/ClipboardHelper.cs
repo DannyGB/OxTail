6a31b8fa540059abe0b0a31960f3f37295ec6842
dannygb <dannygb@7887d560-b91c-4884-a66a-915123185056>
2011-03-08 17:44:15 +0000

    Clipboard refactor
    modified   OxTail/MainWindow.xaml.cs
    modified   OxTailHelpers/OxTail.Helpers.csproj
    added      OxTail/README.txt
    added      OxTailHelpers/ClipboardHelper.cs

---
+++ OxTailHelpers/ClipboardHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OxTailHelpers
+{
+    public static class ClipboardHelper
+    {
+        /// <summary>
+        /// Adds text to clipboard
+        /// </summary>
+        /// <param name="text">The text to add to the clipboard</param>
+        public static void AddTextToClipboard(string text)
+        {
+            // See for try/catch explaination: http://www.switchonthecode.com/tutorials/wpf-tutorial-using-the-clipboard
+            try
+            {
+                Clipboard.SetData(DataFormats.Text, text);
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+                System.Threading.Thread.Sleep(0);
+                try
+                {
+                    Clipboard.SetData(DataFormats.Text, text);
+                }
+                catch (System.Runtime.InteropServices.COMException)
+                {
+                    MessageBox.Show("Can't Access Clipboard");
+                } 
+            }
+        }
+    }
+}
